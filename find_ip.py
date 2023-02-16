a616a52628f525be5f948d2d977dc6ff89947e74
Timofei <84688775+buttheadgg@users.noreply.github.com>
2023-02-16 18:45:58 +0300

    Задача 4
    Программа выводит текущий публичный ip-адрес компьютера

---
+++ find_ip.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+URL = 'https://ifconfig.me/'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+    'upgrade-insecure-requests': '1',
+    'cookie': 'mos_id=CllGxlx+PS20pAxcIuDnAgA=; session-cookie=158b36ec3ea4f5484054ad1fd21407333c874ef0fa4f0c8e34387efd5464a1e9500e2277b0367d71a273e5b46fa0869a; NSC_WBS-QUBG-jo-nptsv-WT-443=ffffffff0951e23245525d5f4f58455e445a4a423660; rheftjdd=rheftjddVal; _ym_uid=1552395093355938562; _ym_d=1552395093; _ym_isad=2'
+}
+def find_id(soup):
+    searched_tag = 'strong'
+    searched_tag_id = 'ip_address'
+    tag = soup.find(searched_tag, {'id': searched_tag_id})
+    print(tag.text)
+
+
+if __name__ == '__main__':
+    parsing_method = 'html.parser'
+    request = requests.get(URL, headers=HEADERS)
+    soup = BS(request.text, parsing_method)
+    find_id(soup)
+
+
